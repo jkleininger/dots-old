d9fb880dd56608b7a34471f6c9f5fc7d3fb6e402
J K Leininger <jkleininger@gmail.com>
2014-03-25 16:20:15 -0400

    now awesome?

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -12,141 +12,70 @@ import XMonad.Hooks.EwmhDesktops
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 
--- The preferred terminal program, which is used in a binding below and by
--- certain contrib modules.
---
 myTerminal      = "roxterm"
 
--- Whether focus follows the mouse pointer.
 myFocusFollowsMouse :: Bool
 myFocusFollowsMouse = False
 
--- Whether clicking on a window to focus also passes the click to the window
 myClickJustFocuses :: Bool
 myClickJustFocuses = True
 
--- Width of the window border in pixels.
---
 myBorderWidth   = 1
 
--- modMask lets you specify which modkey you want to use. The default
--- is mod1Mask ("left alt").  You may also consider using mod3Mask
--- ("right alt"), which does not conflict with emacs keybindings. The
--- "windows key" is usually mod4Mask.
---
 myModMask       = mod4Mask
 
--- The default number of workspaces (virtual screens) and their names.
--- By default we use numeric strings, but any string may be used as a
--- workspace name. The number of workspaces is determined by the length
--- of this list.
---
--- A tagging example:
---
--- > workspaces = ["web", "irc", "code" ] ++ map show [4..9]
---
 myWorkspaces    = ["one", "two", "three", "four", "five"]
 
--- Border colors for unfocused and focused windows, respectively.
---
 myNormalBorderColor  = "#dddddd"
 myFocusedBorderColor = "#ff0000"
 
-customPP = defaultPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">"
-  , ppHidden = xmobarColor "#C98F0A" ""
-    , ppHiddenNoWindows = xmobarColor "#C9A34E" ""
-    , ppUrgent = xmobarColor "#FFFFAF" "" . wrap "[" "]"
-    , ppLayout = xmobarColor "#C9A34E" ""
-    , ppTitle = xmobarColor "#C9A34E" "" . shorten 80
-    , ppSep = xmobarColor "#429942" "" " | "
+customPP = defaultPP { 
+  ppCurrent  = xmobarColor "#429942" "" . wrap "<" ">" ,
+  ppHidden   = xmobarColor "#C98F0A" "" ,
+  ppHiddenNoWindows = xmobarColor "#C9A34E" "" ,
+  ppUrgent   = xmobarColor "#FFFFAF" "" . wrap "[" "]" ,
+  ppLayout   = xmobarColor "#C9A34E" "" ,
+  ppTitle    = xmobarColor "#C9A34E" "" . shorten 80 ,
+  ppSep      = xmobarColor "#429942" "" " | "
 }
-
-
 
 ------------------------------------------------------------------------
 -- Key bindings. Add, modify or remove key bindings here.
 --
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 
-    -- launch a terminal
     [ ((modm,               xK_Return), spawn $ XMonad.terminal conf)
-
-    -- launch dmenu
     , ((modm,               xK_r     ), spawn "dmenu_run")
-
-    -- close focused window
     , ((modm .|. shiftMask, xK_c     ), kill)
-
-     -- Rotate through the available layout algorithms
     , ((modm,               xK_space ), sendMessage NextLayout)
-
-    -- Move focus to the next window
     , ((mod1Mask,           xK_Tab   ), windows W.focusDown)
-
-    -- Swap the focused window with the next window
     , ((modm,               xK_Left  ), windows W.swapDown  )
-
-    -- Swap the focused window with the previous window
     , ((modm,               xK_Right ), windows W.swapUp    )
-
-    -- Push window back into tiling
+    , ((modm,               xK_t     ), spawn "tp-toggle")
+    , ((modm,               xK_b     ), spawn "firefox")
+    , ((modm,               xK_l     ), spawn "lockit")
+    , ((modm,               xK_e     ), spawn "pcmanfm")
+    , ((modm,               xK_n     ), spawn "leafpad")
     -- , ((modm,               xK_t     ), withFocused $ windows . W.sink)
-
-    -- Toggle the status bar gap
-    -- Use this binding with avoidStruts from Hooks.ManageDocks.
-    -- See also the statusBar function from Hooks.DynamicLog.
-    --
-    -- , ((modm              , xK_b     ), sendMessage ToggleStruts)
-
-    -- Quit xmonad
     , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
-
-    -- Restart xmonad
     , ((modm .|. shiftMask, xK_r     ), spawn "xmonad --recompile; xmonad --restart")
-
     ]
     ++
 
-    --
-    -- mod-[1..9], Switch to workspace N
-    -- mod-shift-[1..9], Move client to workspace N
-    --
     [((m .|. modm, k), windows $ f i)
         | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_5]
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
-
-
 
 ------------------------------------------------------------------------
 -- Mouse bindings: default actions bound to mouse events
 --
 myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
-
-    -- mod-button1, Set the window to floating mode and move by dragging
-    [ ((modm, button1), (\w -> focus w >> mouseMoveWindow w
-                                       >> windows W.shiftMaster))
-
-    -- mod-button2, Raise the window to the top of the stack
+    [ ((modm, button1), (\w -> focus w >> mouseMoveWindow w >> windows W.shiftMaster))
     , ((modm, button2), (\w -> focus w >> windows W.shiftMaster))
-
-    -- mod-button3, Set the window to floating mode and resize by dragging
-    , ((modm, button3), (\w -> focus w >> mouseResizeWindow w
-                                       >> windows W.shiftMaster))
-
-    -- you may also bind events to the mouse scroll wheel (button4 and button5)
+    , ((modm, button3), (\w -> focus w >> mouseResizeWindow w >> windows W.shiftMaster))
     ]
-
 ------------------------------------------------------------------------
 -- Layouts:
-
--- You can specify and transform your layouts by modifying these values.
--- If you change layout bindings be sure to use 'mod-shift-space' after
--- restarting (with 'mod-q') to reset your layout state to the new
--- defaults, as xmonad preserves your old layout settings by default.
---
--- The available layouts.  Note that each layout is separated by |||,
--- which denotes layout choice.
---
 myLayout = tiled ||| Mirror tiled ||| Full
   where
      -- default tiling algorithm partitions the screen into two panes
@@ -184,39 +113,17 @@ myManageHook = composeAll
 
 ------------------------------------------------------------------------
 -- Event handling
-
--- * EwmhDesktops users should change this to ewmhDesktopsEventHook
---
--- Defines a custom handler function for X Events. The function should
--- return (All True) if the default handler is to be run afterwards. To
--- combine event hooks use mappend or mconcat from Data.Monoid.
---
 myEventHook = ewmhDesktopsEventHook
 
 ------------------------------------------------------------------------
 -- Status bars and logging
-
--- Perform an arbitrary action on each internal state change or X event.
--- See the 'XMonad.Hooks.DynamicLog' extension for examples.
---
-
 ------------------------------------------------------------------------
 -- Startup hook
-
--- Perform an arbitrary action each time xmonad starts or is restarted
--- with mod-q.  Used by, e.g., XMonad.Layout.PerWorkspace to initialize
--- per-workspace layout choices.
---
--- By default, do nothing.
 myStartupHook = ewmhDesktopsStartup
-
 ------------------------------------------------------------------------
--- Now run xmonad with all the defaults we set up.
 
--- Run xmonad with the settings you specify. No need to modify this.
---
 main = do
-  -- xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmonad/.xmobarrc"
+  xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmonad/.xmobarrc"
   xmonad defaultConfig {
 
         manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig,
@@ -243,12 +150,12 @@ main = do
         startupHook        = myStartupHook,
 
        -- to show workspace and current window in xmobar
-       -- logHook = dynamicLogWithPP xmobarPP {
-       --   ppOutput = hPutStrLn xmproc,
-       --   ppTitle = xmobarColor "blue" "" . shorten 50,
-       --   ppLayout = const ""
-       -- }
+       logHook = dynamicLogWithPP xmobarPP {
+         ppOutput = hPutStrLn xmproc,
+         ppTitle = xmobarColor "blue" "" . shorten 50,
+         ppLayout = const ""
+       }
 
-       logHook = ewmhDesktopsLogHook
+       -- logHook = ewmhDesktopsLogHook
 
     }
